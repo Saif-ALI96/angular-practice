dcbfe163fb545aa201fa00442d41b7cc3845817a
Saif Ali <kattan.saifali@gmail.com>
2023-06-22 15:57:07 +0200

    create model ts for the car

--- carage-de-voiture/src/app/app.module.ts
+++ carage-de-voiture/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewCarComponent } from './new-car/new-car.component';
+import { ListDeVoitureComponent } from './list-de-voiture/list-de-voiture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCarComponent,
+    ListDeVoitureComponent
   ],
   imports: [
     BrowserModule
