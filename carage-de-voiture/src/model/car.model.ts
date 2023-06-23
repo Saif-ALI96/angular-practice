daf16cfe183cba9c0a6ccc2c282b320d31048465
Saif Ali <kattan.saifali@gmail.com>
2023-06-23 09:48:46 +0200

    create model ts

---
+++ carage-de-voiture/src/model/car.model.ts
@@ -0,0 +1,12 @@
+export class car {
+    model !: string;
+    brand !: string;
+    year  !: number;
+    color !: string;
+    weight !: number;
+    price !: number;
+    description !:string;
+    image !:string;
+
+
+}
