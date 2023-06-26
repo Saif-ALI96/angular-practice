6162a41cddaccc17e766327eaed3e13249a7f755
Saif Ali <kattan.saifali@gmail.com>
2023-06-26 15:56:46 +0200

    update the module

--- site-de-commerce/src/app/app.module.ts
+++ site-de-commerce/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import ListDeProduitComponent from './list-de-produit/list-de-produit.component';
 import { ItemComponent } from './item/item.component';
+import { ElementAjouterParIdComponent } from './element-ajouter-par-id/element-ajouter-par-id.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListDeProduitComponent,
-    ItemComponent
+    ItemComponent,
+    ElementAjouterParIdComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
