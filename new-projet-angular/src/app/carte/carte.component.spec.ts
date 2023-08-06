44c0d4b565ab315b4a957e7cc1b0095f8b542cb0
Saif Ali <kattan.saifali@gmail.com>
2023-08-06 21:29:52 +0200

    update the file

--- new-projet-angular/src/app/carte/carte.component.spec.ts
+++ new-projet-angular/src/app/carte/carte.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CarteComponent } from './carte.component';
 
+
 describe('CarteComponent', () => {
   let component: CarteComponent;
   let fixture: ComponentFixture<CarteComponent>;
@@ -14,7 +15,6 @@ describe('CarteComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
